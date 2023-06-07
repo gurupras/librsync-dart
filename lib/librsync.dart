3f4029ea2e500530be85513c55f97874bff7070b
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2023-06-07 07:48:37 -0400

    Export delta_stream.dart

--- lib/librsync.dart
+++ lib/librsync.dart
@@ -4,5 +4,6 @@
 library;
 
 export 'package:librsync/src/reader_writer.dart';
+export 'package:librsync/src/delta_stream.dart';
 
 // TODO: Export any libraries intended for clients of this package.
